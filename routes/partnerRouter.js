5d0822ce9469bfa07cd2c986232e1ba1b3a6746a
Drew Theran <drew.theran@gmail.com>
2020-06-05 19:53:36 -0700

    Express REST API with MongoDB and Mongoose Part 1

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -3,34 +3,50 @@ const bodyParser = require('body-parser');
 
 const partnerRouter = express.Router();
 
-partnerRouter.route('/:partnerId');
+partnerRouter.route('/:partnerId')
 
 partnerRouter.all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
+});
+
+partnerRouter.put('/partners/:partnerId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /partners`);
+});
+
+partnerRouter.delete('/partners/:partnerId', (req, res) => {
+    res.end(`Deleting partner: ${req.params.partnerId}`);
+});
+
+
+partnerRouter.use(bodyParser.json());
+
+partnerRouter.route('/')
+
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
 })
 
-partnerRouter.get('/', (req, res) => {
+.get((req, res) => {
     res.end(`Will send all the partners to you`);
 })
 
-partnerRouter.post('/', (req, res) => {
+.post((req, res) => {
     res.statusCode = 403;
     res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
 })
 
-partnerRouter.put('/:partnerId', (req, res) => {
+.put((req, res) => {
     res.statusCode = 403;
     res.end(`PUT operation not supported on /partners/${req.params.partnerId}`);
 })
 
-partnerRouter.delete('/:partnerId', (req, res) => {
+.delete((req, res) => {
     res.end(`Deleting partner: ${req.params.partnerId}`);
-})
-
-partnerRouter.use(bodyParser.json());
-
-
+});
 
 module.exports = partnerRouter;
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -11,26 +11,40 @@ promotionRouter.all((req, res, next) => {
     next();
 })
 
-promotionRouter.get('/', (req, res) => {
+promotionRouter.put('/promotions/:promotionId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /promotions`);
+});
+
+promotionRouter.delete('/promotions/:promotionId', (req, res) => {
+    res.end(`Deleting promotion: ${req.params.promotionId}`);
+});
+
+promotionRouter.use(bodyParser.json());
+
+promotionRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res) => {
     res.end(`Will send all the promotions to you`);
 })
 
-promotionRouter.post('/', (req, res) => {
+.post((req, res) => {
     res.statusCode = 403;
     res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`);
 })
 
-promotionRouter.put('/:promotionId', (req, res) => {
+.put((req, res) => {
     res.statusCode = 403;
     res.end(`PUT operation not supported on /promotions/${req.params.promotionId}`);
 })
 
-promotionRouter.delete('/:promotionId', (req, res) => {
+.delete((req, res) => {
     res.end(`Deleting promotion: ${req.params.promotionId}`);
-})
-
-promotionRouter.use(bodyParser.json());
-
-
+});
 
 module.exports = promotionRouter;
