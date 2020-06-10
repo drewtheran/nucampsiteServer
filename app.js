972f85ce1432de5012844cd384a56ad840b905db
Drew Theran <drew.theran@gmail.com>
2020-06-10 10:23:47 -0700

    Express Session Part 2

--- app.js
+++ app.js
@@ -45,33 +45,26 @@ app.use(session({
     store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter)
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if(!req.session.user) {
     const authHeader = req.headers.authorization;
-    if (!authHeader) {
       const err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       return next(err);
-    }
-
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === 'admin' && pass === 'password') {
-        return next(); // authorized
-    } else {
+  } else {
+      if (req.session.user ==='authenticated') {
+          return next();
+      } else {
         const err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
         err.status = 401;
         return next(err);
-    }
-  } else {
-    const err = new Error('You are not authenticated!');
-    err.status = 401;
-    return next(err);
+      }
   }
 }
 
